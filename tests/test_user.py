0bdce7fdbcb08e89d4ee62443aa3391ef70a9ef2
RodgersM <mkishagha@gmail.com>
2017-11-21 20:10:34 +0300

    (chore) : add more tests for the signup and login methods

--- tests/test_user.py
+++ tests/test_user.py
@@ -16,3 +16,37 @@ class UserTests(unittest.TestCase):
         """Test is a user with correct credentials can register sucessfully"""
         res = self.user.register("rodger", "654123", "654123")
         self.assertEqual(res, "Registration successfull")
+
+    def test_existing_user(self):
+        """TTest with an already existing user, try registering a user twice"""
+        self.user.register("rodger", "654123", "654123")
+        res = self.user.register("rodger", "654123", "654123")
+        self.assertEqual(res, "Username already exists.")
+
+    def test_password_length(self):
+        res = self.user.register("random", "654", "654")
+        self.assertEqual(res, "Password too short")
+
+    def test_password_match(self):
+        res = self.user.register("random", "654123", "1234654")
+        self.assertEqual(res, "passwords do not match")
+
+    def test_user_login(self):
+        """Test if a user with valid details can login"""
+        self.user.register("rodger", "654123", "654123")
+        res = self.user.login("rodger", "654123")
+        self.assertEqual(res, "successful")
+
+    def test_wrong_password(self):
+        """Test for a login attempt with a wrong password"""
+        self.user.register("rodger", "654123", "654123")
+        res = self.user.login("rodger", "65471234")
+        self.assertEqual(res, "wrong password")
+
+    def test_non_existing_user_login(self):
+        """Test if a non-existing user can login"""
+        self.user.register("rodger", "654123", "654123")
+        res = self.user.login("mzungu", "654123")
+        self.assertEqual(res, "user does not exist")
+
+    
