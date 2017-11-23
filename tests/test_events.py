82cde7b5dab69c9cfcf941ed5ad7707a02a1c9e5
RodgersM <mkishagha@gmail.com>
2017-11-23 05:55:55 +0300

    (test) : add more tests for the added event methods

--- tests/test_events.py
+++ tests/test_events.py
@@ -19,23 +19,69 @@ class Event_tests(unittest.TestCase):
     	self.assertEqual(res, "event created")
 
     def test_crete_existing_event(self):
-    	""" Test if an evnt can be cr"eated twice"""
+    	""" Test if an event can be created twice"""
     	
     	self.event.event_list =[{"name" :'hackathon', "description" :'get experience', "category":'technology', \
     		 "location":'the space', "date" : 26/11/2017, "createdby":'rodger'}]
     	res = self.event.create("hackathon", "get experience", "technology", "the space", 26/11/2017, "rodger")
     	self.assertEqual(res, "event exists")
 
+    def test_same_event_diff_location(self):
+    	""" Test if a user can create the same event but in different locations"""
+    	self.event.event_list =[{"name" :'hackathon', "description" :'get experience', "category":'technology', \
+    		 "location":'the space', "date" : 26/11/2017, "createdby":'rodger'}]
+    	res = self.event.create("hackathon", "get experience", "technology", "the office", 26/11/2017, "rodger")
+    	self.assertEqual(res, "event created")
+
     def test_event_name_length(self):
     	""" Test if one can create an event with less than 3 characters"""
     	res = self.event.create('ab', 'get experience', 'technology', 'the space', 26/11/2017, 'rodger')
-    	self.assertEqual(res, "name too short")
+    	self.assertEqual(res, "name too short or invalid")
 
-    def test_first_event_id(self):
-    	""" Test if id's are generated correctly"""
-    	self.event.create("hackathon", "get experience", "technology", "the space", 26/11/2017, "rodger")
-    	id = self.event.event_list[0]['id']
-    	self.assertEqual(id, 1)
+    def test_category_filter(self):
+    	"""Test if filter lcategory works"""
+    	self.event.create("hackathon", "get experience", "technology", "the office", 26/11/2017, "rodger")
+    	self.event.create("marathon", "run", "sports", "ndakaini", 26/11/2017, "rodger")
+    	res = self.event.category_filter("sports")
+    	event_name = res[0]['name']
+    	self.assertIs(event_name, "marathon")
+    	self.assertIsNot(event_name, "hackathon")
+    	self.assertEqual(len(res), 1)
+    	self.assertIsInstance(res, list)
+
+    def test_location_filter(self):
+    	"""Test if filter location works"""
+    	self.event.create("hackathon", "get experience", "technology", "the office", 26/11/2017, "rodger")
+    	self.event.create("marathon", "run", "sports", "ndakaini", 26/11/2017, "rodger")
+    	res = self.event.location_filter("ndakaini")
+    	event_name = res[0]['name']
+    	self.assertIs(event_name, "marathon")
+    def test_delete(self):
+    	"""Test if given an id the method will delete an event"""
+    	self.event.create("marathon", "run", "sports", "ndakaini", 26/11/2017, "rodger")
+    	event_id = self.event.event_list[0]['id']
+    	res = self.event.delete(event_id)
+    	self.assertIs(res, "deleted")
+    	self.assertEqual(len(self.event.event_list), 0)
+
+    def test_update(self):
+    	"""Test if method can update an event successfully"""
+    	self.event.create("marathon", "run", "sports", "ndakaini", 26/11/2017, "rodger")
+    	event_id = self.event.event_list[0]['id']
+    	#update the location
+    	res = self.event.update(event_id, "marathon", "run", "sports", "mombasa", 26/11/2017, "rodger")
+    	self.assertEqual(res, "update success")
+    	#check if the location has been updated
+    	new_location = self.event.event_list[0]['location']
+    	self.assertIs(new_location, "mombasa")
+    	#check if the id is still the same after update
+    	new_id = self.event.event_list[0]['id']
+    	self.assertEqual(event_id, new_id)
 
 
 
+
+
+
+
+   
