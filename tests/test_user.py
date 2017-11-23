e872eff861a044906f030ef94752c8cb6b66e3d8
RodgersM <mkishagha@gmail.com>
2017-11-23 05:54:53 +0300

    (chore) : remove tests for the id uniqueness
    the id is generated by uuid1 hence the tests are no longer necessary

--- tests/test_user.py
+++ tests/test_user.py
@@ -22,16 +22,6 @@ class UserTests(unittest.TestCase):
         self.user.register("rodger", "654123", "654123")
         res = self.user.register("rodger", "654123", "654123")
         self.assertEqual(res, "Username already exists.")
-    def test_first_user_has_id_1(self):
-        """Test if the id's are generated correctly"""
-        self.user.register("rodger", "654123", "654123")
-        id = self.user.user_list[0]['id']
-        self.assertEqual(id, 1)
-    def test_if_id_is_int(self):
-        """Test if the ids are integers"""
-        self.user.register("rodger", "654123", "654123")
-        id = self.user.user_list[0]['id']
-        self.assertIsInstance(id, int)
 
     def test_password_length(self):
         """Test to ensure that a user has a strong password"""
@@ -42,7 +32,7 @@ class UserTests(unittest.TestCase):
         """Test if password matching is working"""
         res = self.user.register("random", "654123", "1234654")
         self.assertEqual(res, "passwords do not match")
-
+    
     def test_user_login(self):
         """Test if a user with valid details can login"""
         self.user.register("rodger", "654123", "654123")
