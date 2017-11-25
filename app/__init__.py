89b9ba617a680932c4a4b1cdf6279ef3a62ae31e
RodgersM <mkishagha@gmail.com>
2017-11-26 00:33:30 +0300

    (feat) : import middleware and config files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from app import user, events, rsvp, middleware
 
 # Initialize flask app
 app = Flask(__name__, instance_relative_config=True)
+app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
+#load from config.py in root folder
+app.config.from_object('config')
+#load from config.py in instance folder
+app.config.from_pyfile('config.py')
 
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-app.secret_key = 'shhhssshhh'
-
-from app import user, events
-
+#import the user, events and rsvp classes
 user_object = user.User_details()
 event_object = events.Events()
+rsvp_object = rsvp.Rsvp()
 
+#import the views
 from app import views
