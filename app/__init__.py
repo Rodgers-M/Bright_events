b513126eb473b5981e297fce75d60b0f852755fe
RodgersM <mkishagha@gmail.com>
2017-11-28 17:28:40 +0300

    (fix) : specify the correct import of the config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from app import user, events, rsvp, middleware
-from app.config import app_config
+from config import app_config
 
 # Initialize flask app
 app = Flask(__name__, instance_relative_config=True, template_folder='../designs/ui/templates', \
 	static_folder='../designs/ui/static')
-app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
 #load from config.py in root folder
-app.config.from_object(app_config['development'])
+app.config.from_object(app_config['production'])
 #load from config.py in instance folder
-#app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
+#specify application route url
+app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
 
 #import the user, events and rsvp classes
 user_object = user.User_details()
