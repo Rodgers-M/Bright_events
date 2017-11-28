f8bd3fa09d42e3a5e919362e5c277ae6951f0f26
RodgersM <mkishagha@gmail.com>
2017-11-28 07:33:21 +0300

    (feat) : tell the app where to find templates and static files

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from app import user, events, rsvp, middleware
 
 # Initialize flask app
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True, template_folder='../designs/ui/templates', \
+	static_folder='../designs/ui/static')
 app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
 #load from config.py in root folder
 app.config.from_object('config')
