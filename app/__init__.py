e64494d4e835d80d8cd00c526c0559cefe70d078
RodgersM <mkishagha@gmail.com>
2017-11-28 18:10:40 +0300

    (feat) : replace the instance folder config file with .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask
 from app import user, events, rsvp, middleware
+from dotenv import load_dotenv
 from config import app_config
+
 
 # Initialize flask app
 app = Flask(__name__, instance_relative_config=True, template_folder='../designs/ui/templates', \
 	static_folder='../designs/ui/static')
 #load from config.py in root folder
 app.config.from_object(app_config['production'])
-#load from config.py in instance folder
-app.config.from_pyfile('config.py')
+
+# load dotenv in the base root
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
 #specify application route url
 app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
 
