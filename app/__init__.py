d0eb40495c8d3528719d0f74aa8812ffbd2d0c95
RodgersM <mkishagha@gmail.com>
2017-11-28 16:38:42 +0300

    (feat) : import the new config file
    specify the app to be configured from that

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from app import user, events, rsvp, middleware
+from app.config import app_config
 
 # Initialize flask app
 app = Flask(__name__, instance_relative_config=True, template_folder='../designs/ui/templates', \
 	static_folder='../designs/ui/static')
 app.wsgi_app = middleware.PrefixMiddleware(app.wsgi_app, prefix='/api/v1')
 #load from config.py in root folder
-app.config.from_object('config')
+app.config.from_object(app_config['development'])
 #load from config.py in instance folder
 #app.config.from_pyfile('config.py')
 
