71d2f3bde72f1fe0c84126707584a03479beb9d3
RodgersM <mkishagha@gmail.com>
2017-11-29 17:05:29 +0300

    (feat) : modify routes to enable viewing and responding to events

--- app/views.py
+++ app/views.py
@@ -13,15 +13,10 @@ def index():
 def register():
 	"""A route to handle user"""
 	if request.method == 'POST':
-		print('request')
 		username = request.form['username']
-		print(username)
 		email = request.form['email']
-		print(email)
 		password = request.form['password']
-		print(password)
 		cnfpass = request.form['cnfpass']
-		print(cnfpass)
 		#pass the details to the register method
 		res = user_object.register(username, email, password, cnfpass)
 		if res == "Username already exists."\
@@ -45,8 +40,6 @@ def login():
 				if user['username'] == username:
 					session['userid'] = user['id']
 					session['username'] = username
-					print(session['userid'])
-					print(session['username'])
 			flash('login success' , 'success')
 			return redirect(url_for('newevent'))
 		flash("wrong password or username", 'warning')
@@ -76,13 +69,11 @@ def events():
 	events = event_object.view_all()
 	return render_template('events/eventlist.html', events=events)
 
-@app.route('/events/<eventid>', methods = ['GET','PUT'])
+@app.route('/events/<eventid>', methods = ['GET','POST'])
 def update_event(eventid):
 	"""A route to handle event updates"""
-	if request.method == 'PUT':
+	if request.method == 'POST':
 		eventid = uuid.UUID(eventid)
-		event_details = request.get_json()
-		print(eventid)
 		name = request.form['eventname']
 		description = request.form['description']
 		category = request.form['category']
@@ -90,16 +81,34 @@ def update_event(eventid):
 		event_date = request.form['event_date']
 		createdby = session['userid']
 		res = event_object.update(eventid, name,description, category, location, event_date, createdby)
-		return res
-	return render_template('events/edit.html')
+		if res == "Event cannot be updated, a similar event exists" \
+			or res == "name too short or invalid":
+			flash('event can not be udated, please correct the values', 'warning')
+			return redirect(url_for('update_event', eventid = eventid))
+	res = event_object.find_by_id(eventid)
+	if res == "event not found":
+		flash("event not found, it might have been deleted", 'warning')
+		return redirect(url_for('myevents'))
+	return render_template('events/edit.html', event = res)
 
-@app.route('/events/<eventid>', methods=['DELETE'])
+@app.route('/events/myevents')
+def myevents():
+	"""This route returns events belonging to a specific user"""
+	username = session['username']
+	events = event_object.createdby_filter(username)
+	return render_template('events/personalEvents.html', events = events)
+	
+@app.route('/events/<eventid>', methods=['POST'])
 def delete_event(eventid):
 	"""A route to handle deletion of events"""
 	eventid = uuid.UUID(eventid)
 	res = event_object.delete(eventid)
-	return res
-
+	if res == "deleted":
+		flash('event deleted', 'success')
+		return redirect(url_for('myevents'))
+	flash('error, could not delete event')
+	return redirect('myevents')
+	
 @app.route('/event/<eventid>/rsvp', methods=['GET','POST'])
 def rsvp(eventid):
 	"""A route for registering a user to an event"""
@@ -107,7 +116,11 @@ def rsvp(eventid):
 	if request.method == 'POST':
 		userid = session['userid']
 		res = rsvp_object.create(eventid, userid)
-		return res
+		if res == "rsvp success":
+			flash('Successfuly reserved a seat, see you then', 'success')
+			return redirect(url_for('events'))
+		flash('You already registered for this event', 'warning')
+		return redirect(url_for('events'))
 	userids = rsvp_object.view_rsvp(eventid)
 	users = [user for user in user_object.user_list if user['id'] in userids]
 	return jsonify(users)
