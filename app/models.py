d5dc4576fe79632f413263b6408b0e4f26283e54
Rodgers-M <mkishagha@gmail.com>
2017-12-27 10:06:50 +0300

    (feat) : define the data models for the  application

---
+++ app/models.py
@@ -0,0 +1,58 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+rsvps = db.Table('rsvps',
+	db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+	db.Column('event_id', db.Integer, db.ForeignKey('events.id')),
+	)
+
+class User(db.Model):
+	"""this class to defines the user data model"""
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True)
+	email = db.Column(db.String(64), unique=True)
+	password_hash = db.Column(db.String(128))
+	events = db.relationship('Events', backref='created_by')
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+class Events(db.Model):
+	"""This class represents the events table"""
+	__tablename__ = 'events'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64))
+	description = db.Column(db.Text)
+	category = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	author = db.Integer, db.ForeignKey('users.id')
+	event_date = db.Column(db.DateTime)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+	rsvps = db.relationship('User',
+		secondary=rsvps,
+		backref=db.backref('rsvps', lazy='dynamic'),
+		lazy='dynamic')
+
+	def add_rsvp(self, user_id):
+		""" This method adds a user to the list of rsvps"""
+		if not self.has_rsvp(user_id):
+			self.rsvps.append(user_id)
+			db.session.add(self)
+
+	def has_rsvp(self, user_id):
+		"""This method checks if a user is already registered for an event"""
+		return self.rsvps.filter_by(
+			user_id=user_id).first() is not None
+
