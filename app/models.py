9ff9edafba49f7b3f87876a52385f142b5ec04ec
Rodgers-M <mkishagha@gmail.com>
2017-12-28 23:33:24 +0300

    (feat) : remove the some fields and add model methods
    remove the events field from the user model and author from events model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from app import db
 
 rsvps = db.Table('rsvps',
 	db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-	db.Column('event_id', db.Integer, db.ForeignKey('events.id')),
+	db.Column('event_id', db.Integer, db.ForeignKey('events.id'))
 	)
 
 class User(db.Model):
@@ -13,7 +14,6 @@ class User(db.Model):
 	username = db.Column(db.String(64), unique=True)
 	email = db.Column(db.String(64), unique=True)
 	password_hash = db.Column(db.String(128))
-	events = db.relationship('Events', backref=db.backref('created_by'))
 
 	@property
 	def password(self):
@@ -33,26 +33,40 @@ class Events(db.Model):
 	"""This class represents the events table"""
 	__tablename__ = 'events'
 	id = db.Column(db.Integer, primary_key=True)
-	author = db.Column(db.Integer, db.ForeignKey('users.id'))
 	name = db.Column(db.String(64))
+	created_by = db.Column(db.String(64))
 	description = db.Column(db.Text)
 	category = db.Column(db.String(64))
 	location = db.Column(db.String(64))
 	event_date = db.Column(db.DateTime)
 	date_created = db.Column(db.DateTime, default=datetime.utcnow)
-	rsvps = db.relationship('User',
+	rsvps = db.relationship(User,
 		secondary=rsvps,
-		backref=db.backref('rsvps', lazy='dynamic'),
+		backref=db.backref('myrsvps', lazy='dynamic'),
 		lazy='dynamic')
 
 	def add_rsvp(self, user):
 		""" This method adds a user to the list of rsvps"""
-		#if not self.has_rsvp(user.id):
-		self.rsvps.append(user)
-		db.session.add(self)
+		if not self.has_rsvp(user):
+			self.rsvps.append(user)
+			db.session.add(self)
 
 	def has_rsvp(self, user):
 		"""This method checks if a user is already registered for an event"""
 		return self.rsvps.filter_by(
-			user_id=user).first() is not None
+			id=user.id).first() is not None
 
+	@staticmethod
+	def get_all():
+		"""a method to fetch all events"""
+		return Events.query.all()
+
+	@staticmethod
+	def get_user_events(user):
+		"""A method to return events belonging to a particular user"""
+		all_events = Events.query.all()
+		user_events = [event for event in all_events if event.created_by == user.username]
+		return user_events
+
+	def __repr__(self):
+		return '<Events %r>' % self.name
