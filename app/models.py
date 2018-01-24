efdab8436aabef00db58debecba331026cc36e03
Rodgers-M <mkishagha@gmail.com>
2018-01-24 22:51:23 +0300

    (feat ) make the per_page argument a variable

--- app/models.py
+++ app/models.py
@@ -138,6 +138,7 @@ class Events(db.Model):
 	def to_json(self):
 		"""convert a given event to json"""
 		json_event ={
+					"id" : self.id,
 					"name" : self.name,
 					"description" : self.description,
 					"category" : self.category,
@@ -154,24 +155,24 @@ class Events(db.Model):
 		return Events.query.filter_by(id=event_id).first()
 
 	@staticmethod
-	def get_events_by_category(category, page):
+	def get_events_by_category(category, page, per_page):
 		"""filter events by category"""
 		return Events.query.filter(Events.category.ilike("%" + category + "%"))\
 		.filter(cast(Events.event_date, Date) >=  date.today())\
-		.order_by(Events.event_date.desc()).paginate(page, per_page=15, error_out=False)
+		.order_by(Events.event_date.desc()).paginate(page, per_page, error_out=False)
 
 	@staticmethod
-	def get_events_by_location(location, page):
+	def get_events_by_location(location, page, per_page):
 		"""filter events by location"""
 		return Events.query.filter(Events.location.ilike("%" + location + "%"))\
 		.filter(cast(Events.event_date, Date) >=  date.today())\
-		.order_by(Events.event_date.desc()).paginate(page, per_page=15, error_out=False)
+		.order_by(Events.event_date.desc()).paginate(page, per_page, error_out=False)
 
 	@staticmethod
-	def get_events_by_name(name, page):
+	def get_events_by_name(name, page, per_page):
 		"""filter events by name"""
 		return Events.query.filter(Events.name.ilike("%" + name + "%"))\
-		.order_by(Events.event_date.desc()).paginate(page, per_page=15, error_out=False)
+		.order_by(Events.event_date.desc()).paginate(page, per_page, error_out=False)
 
 	#this will be used to test pagination
 	@staticmethod
