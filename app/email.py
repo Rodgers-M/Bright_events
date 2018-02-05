833bbe43344c6aef9709534d0bf0d1c512657d67
Rodgers-M <mkishagha@gmail.com>
2018-02-05 18:50:45 +0300

    (feat) : modify the send_mail function to take subject and template arguments

--- app/email.py
+++ app/email.py
@@ -2,11 +2,13 @@ from flask_mail import Message
 from flask import current_app
 from app import mail
 
-def send_mail(to):
+def send_mail(to, subject, html):
 	"""a function to send emails from the app"""
-	msg = Message("Bright Events", 
+	msg = Message( 
 		sender = current_app.config.get('MAIL_USERNAME'),
-		recipients=[to] )
-	msg.body = "registration successful"
+		recipients=[to]
+		)
+	msg.subject = subject
+	msg.html = html
 	mail.send(msg)
 	return "sent"
