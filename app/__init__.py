6f6b2b4fe8e9dc4277a1b9def68109f9b68c354b
Rodgers-M <mkishagha@gmail.com>
2018-03-26 14:08:20 +0300

    (feat) : import and register error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from config import app_config
+from app.errorhandler import not_found, bad_request, internal_server_error
 
 db = SQLAlchemy()
 mail = Mail()
@@ -17,5 +18,8 @@ def create_app(config_name):
     mail.init_app(app)
     from .api_v2 import api as api_v2_blueprint
     app.register_blueprint(api_v2_blueprint, url_prefix='/api/v2')
+    app.register_error_handler(404, not_found)
+    app.register_error_handler(400, bad_request)
+    app.register_error_handler(500, internal_server_error)
     return app
 	
