4001c20d6014d2ac12dd90e20b619d8cab71a263
Rodgers-M <mkishagha@gmail.com>
2018-04-02 00:46:56 +0300

    [feat]: add docstrings

--- app/email.py
+++ app/email.py
@@ -1,10 +1,11 @@
+""" a module to handle all emailing in the app"""
 from flask_mail import Message
 from flask import current_app
 from app import mail
 
-def send_mail(to, subject, html):
+def send_mail(receiver, subject, html):
     """a function to send emails from the app"""
-    msg = Message(sender = current_app.config.get('MAIL_USERNAME'), recipients = [to])
+    msg = Message(sender = current_app.config.get('MAIL_USERNAME'),recipient=[receiver])
     msg.subject = subject
     msg.html = html
     mail.send(msg)
