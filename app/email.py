dd64ffcc1c77c28d87bcf418f6ea69886250675f
Rodgers-M <mkishagha@gmail.com>
2018-04-05 12:39:58 +0300

    [fix] :fix the argument error in email method

--- app/email.py
+++ app/email.py
@@ -3,9 +3,10 @@ from flask_mail import Message
 from flask import current_app
 from app import mail
 
-def send_mail(receiver, subject, html):
+def send_mail(to, subject, html):
     """a function to send emails from the app"""
-    msg = Message(sender = current_app.config.get('MAIL_USERNAME'),recipient=[receiver])
+    msg = Message(sender =
+            current_app.config.get('MAIL_USERNAME'),recipients=[to])
     msg.subject = subject
     msg.html = html
     mail.send(msg)
