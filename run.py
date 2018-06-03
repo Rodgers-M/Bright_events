796827eb28b51b3510a532abadda189a585c0c13
Rodgers-M <mkishagha@gmail.com>
2018-06-03 08:41:03 +0300

    [feat] : get the port from env or use 7000

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ app = create_app(config_name)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run('', port=port) 
+    port = int(os.environ.get('PORT', 7000))
+    app.run(port=port) 
